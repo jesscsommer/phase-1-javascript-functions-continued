c9669e32e0a99cfc2986e479f0a9fa7a381455be
Jessica Sommerville <jessica.c.sommerville@gmail.com>
2023-04-10 11:05:44 -0700

    Completes lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+function saturdayFun(activityVerb=`roller-skate`){
+    return `This Saturday, I want to ${activityVerb}!`
+}
+
+function mondayWork(workVerb=`go to the office`){
+    return `This Monday, I will ${workVerb}.`
+}
+
+function wrapAdjective(effect="*"){
+    const innerFunction = function(adj="special"){
+        return `You are ${effect}${adj}${effect}!`
+    };
+    return innerFunction;
+}
